31628c635950566e292181d2386b3300c70d5016
Austin Huang <agh3gd@virginia.edu>
2020-08-21 02:09:34 -0400

    Fixed build issue

--- pages/saved.js
+++
@@ -1,4 +0,0 @@
-import styles from "../styles/FlightCard.module.css";
-import Topbar from "../components/Topbar";
-import Layout from "../components/Layout";
-import Footer from "../components/Footer";
