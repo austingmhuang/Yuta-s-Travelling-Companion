e31b3916b7f834f2ede0f8f8cf74474b0f76ef84
Namitry <nobo918@gmail.com>
2020-08-21 10:57:31 +0900

    changed topbar

--- components/Topbar.js
+++ components/Topbar.js
@@ -3,24 +3,26 @@ import styles from "../styles/Topbar.module.css";
 const Topbar = ({ user, loading = false }) => {
   return (
     <div className={styles.Topbar}>
-      <img
-        id={styles.TopbarLogo}
-        src="images/profile.jpg"
-        alt="logo image"
-      ></img>
-      {!loading &&
-        (user ? (
-          <>
-            <a href="/api/logout">
-              Logout{" "}
-              <i class="fas fa-sign-out-alt" className={styles.login}></i>
+      <div className={styles.wrapper}>
+        <img
+          id={styles.TopbarLogo}
+          src="images/profile.jpg"
+          alt="logo image"
+        ></img>
+        {!loading &&
+          (user ? (
+            <>
+              <a href="/api/logout">
+                Logout{" "}
+                <i class="fas fa-sign-out-alt" className={styles.login}></i>
+              </a>
+            </>
+          ) : (
+            <a href="/api/login">
+              Login <i class="fas fa-sign-in-alt" className={styles.login}></i>
             </a>
-          </>
-        ) : (
-          <a href="/api/login">
-            Login <i class="fas fa-sign-in-alt" className={styles.login}></i>
-          </a>
-        ))}
+          ))}
+      </div>
     </div>
   );
 };
