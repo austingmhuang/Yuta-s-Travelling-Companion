bb65f9d8bfb52d24a1594c85edce53264b030ace
Namitry <nobo918@gmail.com>
2020-08-21 16:37:34 +0900

    changed covers

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,9 @@ const Footer = () => {
   return (
     <div className={styles.footer}>
       <img id={styles.footerLogo} src="images/profile.jpg"></img>
+      <p className={styles.copyright}>
+        ©2020 Yuta's<br></br>Travel Companion{" "}
+      </p>
     </div>
   );
 };
