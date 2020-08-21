3f9a38de9749d69a5d13194bb2253e2804401407
Ryukyo <florian.fejer@hotmail.de>
2020-08-21 03:45:27 +0200

    New loading way working for fixed result

---
+++ pages/hotel.js
@@ -0,0 +1,39 @@
+// This import is only needed when checking authentication status directly from getInitialProps
+// import auth0 from '../lib/auth0'
+import { useFetchHotel } from "../lib/hotels";
+import Layout from "../components/layout";
+import { useFetchUser } from "../lib/user";
+import styles from "../styles/HotelCard.module.css";
+import Spinner from "../components/Spinner";
+
+function HotelCard({ hotel }) {
+  console.log("hotel in page", hotel);
+  return (
+    <div className={styles.card}>
+      <img
+        id={styles.hotelImg}
+        src="https://exp.cdn-hotels.com/hotels/20000000/19800000/19794400/19794327/b02255b8_l.jpg"
+        alt="logo image"
+        className={styles.img}
+      ></img>
+      <h3 className={styles.hotelName}>{hotel[0].name}</h3>
+      <p>Rating: {hotel[0].starRating} </p>
+      <p>Address: {hotel[0].address.streetAddress} </p>
+      <p>Price: {hotel[0].ratePlan.price.current} </p>
+    </div>
+  );
+}
+function Hotel() {
+  const { hotel, loadingHotel } = useFetchHotel();
+  const { user, loading } = useFetchUser();
+  return (
+    <Layout user={user} loading={loading}>
+      {loadingHotel ? (
+        <Spinner />
+      ) : (
+        <HotelCard hotel={hotel.data.body.searchResults.results} />
+      )}
+    </Layout>
+  );
+}
+export default Hotel;
